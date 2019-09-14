6259e68b48c94cfdbaf87e58129e8443bdf35912
tacotoemeck <53991292+tacotoemeck@users.noreply.github.com>
2019-09-14 23:29:24 +0100

    Add files via upload

---
+++ removeStringSpaces.js
@@ -0,0 +1,9 @@
+/*
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+function noSpace(x){
+return x.replace(/\s/g,'')
+}
+
+noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')
